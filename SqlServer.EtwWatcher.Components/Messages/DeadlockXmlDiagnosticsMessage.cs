7f07f536151192475afb1fa8d77148689d805bf4
Andrew Warren <a89.warren@gmail.com>
2019-10-17 11:16:07 -0500

    removed wrong files

---
+++ SqlServer.EtwWatcher.Components/Messages/DeadlockXmlDiagnosticsMessage.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace SqlServer.EtwWatcher.Components.Messages
+{
+    /// <summary>
+    /// Diagnostics Message representing Deadlock Extended Event
+    /// </summary>
+    public class DeadlockXmlDiagnosticsMessage
+    {
+        public DateTime TimeGenerated { get; set; }
+
+        public string BlockType { get; set; }
+
+        public string Server { get; set; }
+
+        public string Process1_Id { get; set; }
+
+        public DateTime Process1_EventTime { get; set; }
+
+        public string Process1_IsVictim { get; set; }
+
+        public string Process1_Database { get; set; }
+
+        public string Process1_Query { get; set; }
+
+        public string Process1_LockMode { get; set; }
+
+        public string Process1_ClientApp { get; set; }
+
+        public string Process1_HostName { get; set; }
+
+        public string Process1_LoginName { get; set; }
+
+        public string Process1_Spid { get; set; }
+
+        public string Process1_Table { get; set; }
+
+        public string Process1_Index { get; set; }
+
+        public string Process2_Id { get; set; }
+
+        public DateTime Process2_EventTime { get; set; }
+
+        public string Process2_IsVictim { get; set; }
+
+        public string Process2_Database { get; set; }
+
+        public string Process2_Query { get; set; }
+
+        public string Process2_LockMode { get; set; }
+
+        public string Process2_ClientApp { get; set; }
+
+        public string Process2_HostName { get; set; }
+
+        public string Process2_LoginName { get; set; }
+
+        public string Process2_Spid { get; set; }
+
+        public string Process2_Table { get; set; }
+
+        public string Process2_Index { get; set; }
+    }
+}
---
+++ SqlServer.EtwWatcher.Components/Messages/DiagnosticsMessageType.cs
@@ -0,0 +1,10 @@
+﻿namespace SqlServer.EtwWatcher.Components.Messages
+{
+    /// <summary>
+    /// Representation of the different types of Diagnostics Message types that could be generated from Sql Server Extended Events
+    /// </summary>
+    public static class DiagnosticsMessageType
+    {
+        public const string DeadlockXmlMessage = "Deadlock";
+    }
+}
